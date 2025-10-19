921a49d84ddb112727cad6aad62f7eaab48457e7
wensheng <13757048+tsukihikari@user.noreply.gitee.com>
2025-10-19 17:47:16 +0800

    增删改查和错误处理

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -23,7 +23,7 @@ export default function EditInvoiceForm({
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
   return (
-    <form action={updateInvoiceWithId as any}>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
